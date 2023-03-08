72a3d29ca204db3fe62883f71bf8d64e837f0cd7
yvenxx <yvenxx@163.com>
2023-03-08 21:44:17 +0800

    first repo

---
+++ src/main/java/cn/yvenxx/zhima_community/ZhimaCommunityApplication.java
@@ -0,0 +1,13 @@
+package cn.yvenxx.zhima_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhimaCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhimaCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/yvenxx/zhima_community/ZhimaCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yvenxx.zhima_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhimaCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
