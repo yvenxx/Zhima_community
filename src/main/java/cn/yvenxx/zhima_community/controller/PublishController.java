fe67fdb09de301e5872fad5ca4f4aa7e3301a1d6
yvenxx <yvenxx@163.com>
2023-03-13 22:17:38 +0800

    add login check and publish

--- src/main/java/cn/yvenxx/zhima_community/controller/PublishController.java
+++ src/main/java/cn/yvenxx/zhima_community/controller/PublishController.java
@@ -10,5 +10,7 @@ public class PublishController {
 
         return "publish";
     }
+
+
 }
 
--- src/test/java/cn/yvenxx/zhima_community/ZhimaCommunityApplicationTests.java
+++ src/test/java/cn/yvenxx/zhima_community/ZhimaCommunityApplicationTests.java
@@ -1,13 +1,22 @@
 package cn.yvenxx.zhima_community;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class ZhimaCommunityApplicationTests {
 
+    @Autowired
+    StringRedisTemplate redisTemplate;
     @Test
     void contextLoads() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("hello","world");
+        String s = ops.get("hello");
+        System.out.println(s);
     }
 
 }
